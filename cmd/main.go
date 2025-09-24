ba83a63d9fce8e904a07851a50e1c977be9b8815
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-09-24 17:39:51 +0300

    Добавлена сервис работы с переменными окружения

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/viacheslav-korobeynikov/rally-app/config"
+)
 
 func main() {
+	// Инициализация чтения файла конфигурации
+	config.Init()
 	// Создание инстанса приложения Fiber
 	app := fiber.New()
 	//Настраиваем порт, который будем слушать
---
+++ config/env.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+// Инициализация загрузки файла окружения
+func Init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file")
+		return
+	}
+	log.Println(".env file loaded")
+}
+
+// Работа с дефолтными значениями конфигов
+func getString(key, defaultValue string) string {
+	// Читаем из переменной окружения
+	value := os.Getenv(key)
+	// Если значение пустое
+	if value == "" {
+		// Возвращаем дефолтное значение
+		return defaultValue
+	}
+	// Если значение не пустое - возвращаем значение переменной окружения
+	return value
+}
+
+// Работа с числовыми значениями
+func getInt(key string, defaultValue int) int {
+	value := os.Getenv(key)
+	// Конвертируем значение в Int
+	i, err := strconv.Atoi(value)
+	if err != nil {
+		return defaultValue
+	}
+	return i
+}
+
+// Работа с булево значениями
+func getBool(key string, defaultValue bool) bool {
+	value := os.Getenv(key)
+	b, err := strconv.ParseBool(value)
+	if err != nil {
+		return defaultValue
+	}
+	return b
+}
