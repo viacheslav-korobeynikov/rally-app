2cb789384a5ceccbfd58156eb2eabbfb3ba0cb41
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-09-24 17:15:22 +0300

    Создан простой сервер на fiber

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "github.com/gofiber/fiber/v2"
 
+func main() {
+	// Создание инстанса приложения Fiber
+	app := fiber.New()
+	//Настраиваем порт, который будем слушать
+	app.Listen(":3000")
 }
