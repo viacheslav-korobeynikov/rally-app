428ec87cecfabed26d7ddd59341f9a623a16f8d3
Viacheslav Korobeynikov <korobeynikovvo@gmail.com>
2025-10-25 09:59:52 +0300

    Добавлена зависимость для главной страницы

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/viacheslav-korobeynikov/rally-app/config"
+	"github.com/viacheslav-korobeynikov/rally-app/internal/home"
 	"github.com/viacheslav-korobeynikov/rally-app/pkg/logger"
 )
 
@@ -21,6 +22,8 @@ func main() {
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: customLogger,
 	}))
+	// Добавлена зависимость с хендлером для главной страницы
+	home.NewHandler(app, customLogger)
 	//Настраиваем порт, который будем слушать
 	app.Listen(":3000")
 }
